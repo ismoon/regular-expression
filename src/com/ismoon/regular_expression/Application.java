4a7ac1c324807c6c0b53d5e1382769fded476905
ismoon <insu4341@gmail.com>
2018-01-15 22:07:52 +0900

    regular-expression : 출력 문구 수정

--- src/com/ismoon/regular_expression/Application.java
+++ src/com/ismoon/regular_expression/Application.java
@@ -13,7 +13,7 @@ public class Application {
 
         //UserName 체크 정규식 (영문자, 숫자, 언더스코어( _ ), 하이픈( - ) 3 ~ 6 자리)
         System.out.println("==============================================");
-        System.out.println("UserName AbstractRegexpChecker : ^[a-z0-9_-]{3,16}$\n");
+        System.out.println("UserName RegexpChecker : ^[a-z0-9_-]{3,16}$\n");
         regexpChecker.setRegexp("^[a-z0-9_-]{3,16}$");
         regexpChecker.checkRegexp("ismoon");
         regexpChecker.checkRegexp("무닌수");
@@ -24,7 +24,7 @@ public class Application {
 
         //Password 체크 정규식 문자, 숫자, 특수문자 포함 8 ~ 16자리
         System.out.println("==============================================");
-        System.out.println("Password AbstractRegexpChecker : ^[a-zA-Z0-9~!@#$%^&*()]{8,16}\n");
+        System.out.println("Password RegexpChecker : ^[a-zA-Z0-9~!@#$%^&*()]{8,16}\n");
         regexpChecker.setRegexp("^[a-zA-Z0-9~!@#$%^&*()]{8,16}");
         regexpChecker.checkRegexp("dfexxf@#$%%^_");
         regexpChecker.checkRegexp("mp3sjanwhgdk@$");
@@ -32,7 +32,7 @@ public class Application {
 
         //Email 체크 정규식
         System.out.println("==============================================");
-        System.out.println("Email AbstractRegexpChecker : ^[a-z0-9A-Z_-]*@[a-z0-9A-Z]*.[a-zA-Z.]*$\n");
+        System.out.println("Email RegexpChecker : ^[a-z0-9A-Z_-]*@[a-z0-9A-Z]*.[a-zA-Z.]*$\n");
         regexpChecker.setRegexp("^[a-z0-9A-Z_-]*@[a-z0-9A-Z]*.[a-zA-Z.]*$");
         regexpChecker.checkRegexp("mjis4341@naver.com");
         regexpChecker.checkRegexp("mjis4341@naver.@com");
@@ -45,7 +45,7 @@ public class Application {
 
         //FileExtensions 체크 정규식
         System.out.println("==============================================");
-        System.out.println("FileExtensions AbstractRegexpChecker : ^\\S+.(?i)(png|jpg|bmp|gif)$\n");
+        System.out.println("FileExtensions RegexpChecker : ^\\S+.(?i)(png|jpg|bmp|gif)$\n");
         regexpChecker.setRegexp("^\\S+.(?i)(png|jpg|bmp|gif)$");
         regexpChecker.checkRegexp("insu.jpg");
         regexpChecker.checkRegexp("INSU.JPG");
@@ -56,7 +56,7 @@ public class Application {
 
         //HexValue 체크 정규식
         System.out.println("==============================================");
-        System.out.println("HexValue AbstractRegexpChecker : ^#?([a-f0-9]{6}|[a-f0-9]{3})$\n");
+        System.out.println("HexValue RegexpChecker : ^#?([a-f0-9]{6}|[a-f0-9]{3})$\n");
         regexpChecker.setRegexp("^#?([a-f0-9]{6}|[a-f0-9]{3})$");
         regexpChecker.checkRegexp("#aaaaaa");
         regexpChecker.checkRegexp("aaaaaa");
@@ -69,7 +69,7 @@ public class Application {
 
         //IP Number 체크 정규식
         System.out.println("==============================================");
-        System.out.println("IP Number AbstractRegexpChecker : ^\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}$\n");
+        System.out.println("IP Number RegexpChecker : ^\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}$\n");
         regexpChecker.setRegexp("^\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}$");
         //regexpChecker.setRegexp("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
         regexpChecker.checkRegexp("192.182.0.12");
@@ -82,7 +82,7 @@ public class Application {
 
         //Slug 체크 정규식
         System.out.println("==============================================");
-        System.out.println("Slug AbstractRegexpChecker : ^[a-z0-9-]+$\n");
+        System.out.println("Slug RegexpChecker : ^[a-z0-9-]+$\n");
         regexpChecker.setRegexp("^[a-z0-9-]+$");
         regexpChecker.checkRegexp("my-title-here");
         regexpChecker.checkRegexp("my_title_here");
@@ -90,7 +90,7 @@ public class Application {
 
         //URL 체크 정규식
         System.out.println("==============================================");
-        System.out.println("URL AbstractRegexpChecker : ^(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w_\\.-]*)*\\/?$\n");
+        System.out.println("URL RegexpChecker : ^(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w_\\.-]*)*\\/?$\n");
         regexpChecker.setRegexp("^(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w_\\.-]*)*\\/?$");
         regexpChecker.checkRegexp("https://www.naver.com/");
         regexpChecker.checkRegexp("http://www.naver.com");
@@ -103,7 +103,7 @@ public class Application {
 
         //Phone Number 체크 정규식
         System.out.println("==============================================");
-        System.out.println("Phone Number AbstractRegexpChecker : (01[016789])(\\d{3,4})(\\d{4})\n");
+        System.out.println("Phone Number RegexpChecker : (01[016789])(\\d{3,4})(\\d{4})\n");
         regexpChecker.setRegexp("(01[016789])(\\d{3,4})(\\d{4})");
         regexpChecker.checkRegexp("01041794341");
         regexpChecker.checkRegexp("010417294341");
@@ -119,7 +119,7 @@ public class Application {
 
         //Resident Registration Number 체크 정규식
         System.out.println("==============================================");
-        System.out.println("RRN AbstractRegexpChecker : ^(\\d{6})([1-4]\\d{6})$\n");
+        System.out.println("RRN RegexpChecker : ^(\\d{6})([1-4]\\d{6})$\n");
         regexpChecker.setRegexp("^(\\d{6})([1-4]\\d{6})$");
         //regexpChecker.setRegexp("^\\dz{6}(1|2|3|4)\\d{6}$");
         //regexpChecker.setRegexp("^\\d{6}[1-4]\\d{6}$");
@@ -134,7 +134,7 @@ public class Application {
 
         // HTML Tag 체크 정규식
         System.out.println("==============================================");
-        System.out.println("HTML Tag AbstractRegexpChecker : ^<([a-z]+)([^<]+)*(?:>(.*)<\\/\\1>|\\s+\\/>)$\n");
+        System.out.println("HTML Tag RegexpChecker : ^<([a-z]+)([^<]+)*(?:>(.*)<\\/\\1>|\\s+\\/>)$\n");
         regexpChecker.setRegexp("^<([a-z]+)([^<]+)*(?:>(.*)<\\/\\1>|\\s+\\/>)$");
         regexpChecker.checkRegexp("<a href=\"http://net.tutsplus.com/\"> Nettuts+</a>");
         regexpChecker.checkRegexp("<img src=\"img.jpg\" alt=\"My image>\" />");
